492ae7644ef49198e6cf446dac627f868332f012
kiranmahale9 <kiranmahale9@gmail.com>
2015-05-25 17:01:50 +0530

    javascript first fiddle

---
+++ fiddle.js
@@ -0,0 +1,11 @@
+function sum(){
+    var sum=0;
+    for(var i=0;i<=1000;i++){
+        if(i%3==0 || i%5==0){
+            sum=sum+i;
+        }
+    }
+
+    return sum;
+}
+document.getElementById("demo").innerHTML = sum();
